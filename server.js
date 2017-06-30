f3ba8fa4e833a8778f13db72cc341761f0b480d7
Lplenka <laluprasad.lenka95@gmail.com>
2017-06-30 22:06:55 +0530

    Bug fix in server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 // app.use(express.static(path.join(__dirname, '/public')));
-// app.use(express.static(path.join(__dirname, '/public/app')));
+app.use(express.static(path.join(__dirname, '/public/app')));
 app.use('/',router);
