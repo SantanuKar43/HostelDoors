c1bc76b5e9d8e3638074fac7a8a9db05bdd7be0d
SantanuKar43 <kar.santanu43@gmail.com>
2017-07-02 13:10:29 +0530

    forgot what changes i had made

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ var express = require('express'),
     }),
     router = require('./routes/index'),
     path = require('path'),
-    bodyParser = require('body-parser');
+    fs = require('fs'),
+    bodyParser = require('body-parser'),
+    papa = require('papaparse'),
+    mongoclient = require("mongodb").MongoClient;
+
+mongoclient.connect("mongodb://localhost:27017/hostels",function(err,db){
+  if(err) return console.log(err);
+  global.db = db
+  db.collection('residents').drop();
+  fs.readFile(__dirname+"/csv.csv",function(err,data){
+    var csvString = data.toString();
+    var results = papa.parse(csvString,{header:true}).data;
+    results.forEach(data=>{db.collection('residents').save(data); console.log(data);})
+  });
+});
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
