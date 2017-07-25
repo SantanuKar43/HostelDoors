72e948b5f2b17070f94ad270872340fc3f12ca80
Lplenka <laluprasad.lenka95@gmail.com>
2017-07-25 10:21:43 +0530

    Autocomplete js and css added

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angucomplete-alt'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -10,5 +10,27 @@
 angular.module('hostelDoorsApp')
   .controller('MainCtrl', function ($scope) {
 
+           $scope.countries = [
+     {name: 'Afghanistan', code: 'AF'},
+     {name: 'Aland Islands', code: 'AX'},
+     {name: 'Albania', code: 'AL'},
+     {name: 'Algeria', code: 'DZ'},
+     {name: 'American Samoa', code: 'AS'},
+     {name: 'AndorrA', code: 'AD'},
+     {name: 'Angola', code: 'AO'},
+     {name: 'Anguilla', code: 'AI'},
+     {name: 'Antarctica', code: 'AQ'},
+     {name: 'Antigua and Barbuda', code: 'AG'},
+     {name: 'Argentina', code: 'AR'},
+     {name: 'Armenia', code: 'AM'},
+     {name: 'Aruba', code: 'AW'},
+     {name: 'Australia', code: 'AU'},
+     {name: 'Austria', code: 'AT'},
+     {name: 'Azerbaijan', code: 'AZ'},
+     {name: 'Bahamas', code: 'BS'},
+     {name: 'Bahrain', code: 'BH'},
+     {name: 'Bangladesh', code: 'BD'},
+     {name: 'Barbados', code: 'BB'}
+     ]
 
   });
