31a5c21e91a9a4c630f269b0c293d5a9194a3713
Lplenka <laluprasad.lenka95@gmail.com>
2017-07-26 12:42:41 +0530

    Complete materialize no bootstrap

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -10,6 +10,9 @@
 angular.module('hostelDoorsApp')
   .controller('MainCtrl', function ($scope) {
 
+            $(".button-collapse").sideNav();
+
+
            $scope.residents= [
      {name: 'Lalu Prasad Lenka', Room: '406'},
      {name: 'Sinchan Kumar Mishra', Room: '306'},
@@ -20,7 +23,7 @@ angular.module('hostelDoorsApp')
      {name: 'Tejaswini Behera', Room: '105'},
      {name: 'Nibedita Jena', Room: '105'},
      {name: 'Sanskriti Kar', Room: '104'},
-     {name: 'Praptti Panigrahi', Room: '103'},
+     {name: 'Prapti Panigrahi', Room: '103'},
      {name: 'Sanjay Reddy', Room: '421'},
      {name: 'Aswini Padhi', Room: '421'}
      ]
